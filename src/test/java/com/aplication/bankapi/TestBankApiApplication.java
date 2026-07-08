08f95e54b93ac0e0b691b99e172f383c31e033bb
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2026-07-08 14:40:55 -0300

    innitial commit

---
+++ src/test/java/com/aplication/bankapi/TestBankApiApplication.java
@@ -0,0 +1,11 @@
+package com.aplication.bankapi;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BankApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
