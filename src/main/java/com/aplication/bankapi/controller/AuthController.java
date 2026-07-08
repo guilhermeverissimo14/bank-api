564a395e1ed564047e5573b031ef73f06c138ed4
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2026-07-08 20:15:50 -0300

    feat: add AuthController

---
+++ src/main/java/com/aplication/bankapi/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.aplication.bankapi.controller;
+
+import com.aplication.bankapi.dto.auth.LoginRequest;
+import com.aplication.bankapi.dto.auth.LoginResponse;
+import com.aplication.bankapi.service.auth.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
+
