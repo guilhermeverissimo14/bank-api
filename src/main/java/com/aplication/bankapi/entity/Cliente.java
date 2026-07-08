3be1068405cc5dbeed2ff65b1d3279bbe55ff966
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2026-07-08 16:49:27 -0300

    feat: add entity Cliente

---
+++ src/main/java/com/aplication/bankapi/entity/Cliente.java
@@ -0,0 +1,45 @@
+package com.aplication.bankapi.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cliente", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_cliente_email", columnNames = "email")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @Column(name = "id")
+    @EqualsAndHashCode.Include 
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 120)
+    private String nome;
+
+    @Column(name = "email", nullable = false, length = 60)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+     @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
