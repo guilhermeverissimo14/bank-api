2b48b047130dcf8000e43df4c34726d283ad2cd9
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2026-07-08 17:49:59 -0300

    feat: add clientRepository

--- src/main/java/com/aplication/bankapi/repository/ClienteRepository.java
+++ src/main/java/com/aplication/bankapi/repository/ClienteRepository.java
@@ -1,5 +1,16 @@
 package com.aplication.bankapi.repository;
 
-public class ClienteRepository {
-    
+import com.aplication.bankapi.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+    List<Cliente> findByNomeContainingIgnoreCase(String nome);
+
 }
